b6c55358d9ef73f45a460be1c6773b74a5db69c0
Sam La <sla@wpi.edu>
2016-01-21 19:20:28 -0500

    Constants for cases of input

--- Player/src/referee/Player.java
+++ Player/src/referee/Player.java
@@ -9,9 +9,13 @@ import java.util.List;
 
 public class Player {
 
-	String playerName="AI Player";
+	String playerName="AI_Player";
 	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	boolean first_move=false;
+	
+	private static final int GAME_OVER = 1;
+	private static final int PLAYER_NAMES = 4;
+	private static final int GAME_INFO = 5;
 	
 	public void processInput() throws IOException{	
 	
@@ -20,14 +24,14 @@ public class Player {
 		if(ls.size()==2){
 			System.out.println(ls.get(0)+" "+ls.get(1));
 		}
-		else if(ls.size()==1){
+		else if(ls.size() == GAME_OVER){
 			System.out.println("game over!!!");
 			System.exit(0);
 		}
-		else if(ls.size()==5){          //ls contains game info
+		else if(ls.size() == GAME_INFO){          //ls contains game info
 			System.out.println("0 1");  //first move
 		}
-		else if(ls.size()==4){		//player1: aa player2: bb
+		else if(ls.size() == PLAYER_NAMES){		//player1: aa player2: bb
 			//TODO combine this information with game information to decide who is the first player
 		}
 		else
