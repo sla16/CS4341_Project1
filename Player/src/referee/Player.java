0f8edd03201767ad8235b4ef4ea8b8b7208a8e15
DIRECTIONNZ <ZhangNan@AUTOREG-713833.WIFI.WPI.EDU>
2016-01-24 13:51:06 -0500

    Unfinished Heuristic

--- Player/src/referee/Player.java
+++ Player/src/referee/Player.java
@@ -27,10 +27,21 @@ public class Player {
 	
     	String s=input.readLine();	
 		List<String> ls=Arrays.asList(s.split(" "));
+		int rows = Integer.parseInt(ls.get(0));
+		int columns = Integer.parseInt(ls.get(1));
+
+
 		if(ls.size() == PLAYER_TURN){
-			updateBoard(Integer.parseInt(ls.get(0)), Integer.parseInt(ls.get(1)), false);
+			updateBoard(this.currentBoard,rows, columns, false);
 			// TODO: Find out the best move and make it
 			//updateBoard(bestMove, bestMove, true);
+			
+			
+			for (int i = 0; i < columns; i++){
+				if(this.currentBoard[rows-1][i] == 9){
+					int[][] heuristicBoard = this.currentBoard;
+				}
+			}
 			int randomMove = (int) Math.floor(Math.random() * this.currentBoard[0].length);
 			System.out.println(randomMove+" 1");
 		}
@@ -44,7 +55,7 @@ public class Player {
 			this.secondsToPlay = Integer.parseInt(ls.get(4));
 			
 			// Initializes the (n x m) board and fills it with 9
-			this.currentBoard = new int[Integer.parseInt(ls.get(0))][Integer.parseInt(ls.get(1))];
+			this.currentBoard = new int[rows][columns];
 			for (int[] row : this.currentBoard) {
 				Arrays.fill(row, 9);
 			}
@@ -53,14 +64,13 @@ public class Player {
 			// If the second player goes first and we are the second player, go first
 			if ((ls.get(3).equals("1") && this.isFirstPlayer) || (ls.get(3).equals("2") && !this.isFirstPlayer)) {
 				// TODO: What move should we make if we are first?
-				int column = Integer.parseInt(ls.get(1));
-				if (column % 2 == 0){
-					updateBoard(column/2, 1, true);
-					System.out.println(column/2 + " 1");
+				if (columns % 2 == 0){
+					updateBoard(this.currentBoard,columns/2, 1, true);
+					System.out.println(columns/2 + " 1");
 				} 
 				else{
-					updateBoard((int) (column/2 + 0.5), 1, true);
-					System.out.println((int)(column/2 + 0.5) + " 1");
+					updateBoard(this.currentBoard,(int) (columns/2 + 0.5), 1, true);
+					System.out.println((int)(columns/2 + 0.5) + " 1");
 				}
 				
 			}
@@ -82,15 +92,15 @@ public class Player {
 	 * @param action The action as place or pop as an int
 	 * @param isMyTurn True or false to update if it is my turn
 	 */
-	public void updateBoard(int position, int action, boolean isMyTurn) {
+	public void updateBoard(int[][] board, int position, int action, boolean isMyTurn) {
 		if(action == PUSH)
 		{
-			for (int i = this.currentBoard.length-1; i > -1; i--) {
-				if (this.currentBoard[i][position] == 9) {
+			for (int i = board.length-1; i > -1; i--) {
+				if (board[i][position] == 9) {
 					if (isMyTurn) {
-						this.currentBoard[i][position] = this.isFirstPlayer ? 1 : 2;
+						board[i][position] = this.isFirstPlayer ? 1 : 2;
 					} else {
-						this.currentBoard[i][position] = this.isFirstPlayer ? 2 : 1;
+						board[i][position] = this.isFirstPlayer ? 2 : 1;
 					}
 					break;
 				}
@@ -99,14 +109,167 @@ public class Player {
 		else 
 		{
 			// Moves all the pieces in that position/column down one row
-			for (int i = this.currentBoard.length-1; i > -1; i--) {
+			for (int i = board.length-1; i > -1; i--) {
 				if (i == 0) {
-					this.currentBoard[i][position] = 9;
+					board[i][position] = 9;
 				} else {
-					this.currentBoard[i][position] = this.currentBoard[i-1][position];
+					board[i][position] = board[i-1][position];
 				}
 			}
 		}
+	}
+	
+//	public void maxMove(int[][] board){
+//		int[][] heuristicBoard = this.currentBoard;
+//		int i = 0;
+//		for (int i = 0; i < Integer.parseInt(ls.get(0)); i++){
+//			for(int )
+//		}
+//		for (heuristicBoard.length > )
+//		Integer.parseInt(ls.get(0));
+//	}
+	
+	public int getHeuristicValue(int[][] board, boolean isMyTurn){
+		int value = 0;
+		int heuristicRow = board.length;
+		int heuristicCol = board[0].length;
+		if (heuristicRow > 1){
+			if (heuristicCol > 1){
+				
+				if (board[0][0] == (isMyTurn ? 2 : 1)){
+					value += 1;
+					if (board[0][1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[1][1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[1][0] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}	
+				}
+				
+				if (board[heuristicRow - 1][heuristicCol - 1] == (isMyTurn ? 2 : 1)){
+					value +=1;
+					if (board[heuristicRow-1][heuristicCol-2] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[heuristicRow-2][heuristicCol-2] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[heuristicRow-2][heuristicCol-1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+				}
+				
+				if (board[heuristicRow - 1][0] == (isMyTurn ? 2 : 1)){
+					value += 1;
+					if (board[heuristicRow-1][1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[heuristicRow-2][0] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[heuristicRow-2][1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+				}
+				
+				if (board[0][heuristicCol - 1] == (isMyTurn ? 2 : 1)){
+					value += 1;
+					if (board[0][heuristicCol-2] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[1][heuristicCol-2] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+					if (board[1][heuristicCol-1] ==(isMyTurn ? 2 : 1)){
+						value += 1;
+					}
+				}
+				
+			}
+			else{
+				if (board[0][0] == (isMyTurn ? 2 : 1)){
+					value += 1;
+					if (board[1][0] == (isMyTurn ? 2: 1)){
+						value += 1;
+					}
+				}
+				
+				if (board[heuristicRow-1][0] == (isMyTurn ? 2: 1)){
+					value += 1;
+					if (board[heuristicRow-2][0] == (isMyTurn ? 2: 1)){
+						value += 1;
+					}
+				}
+			}
+		}
+		else{
+			if (board[0][0] == (isMyTurn ? 2 : 1)){
+				value += 1;
+				if (board[0][1] == (isMyTurn ? 2: 1)){
+					value += 1;
+				}
+			}
+			if (board[0][heuristicCol-1] == (isMyTurn ? 2: 1)){
+				value += 1;				
+				if (board[0][heuristicCol-2] == (isMyTurn ? 2: 1)){
+					value += 1;
+				}
+			}
+		}
+		
+		if (heuristicRow >2){
+			if (heuristicCol > 2){
+				for(int n = 1; n < (heuristicRow -1); n++){
+					if (board[n][0] == (isMyTurn ? 2: 1)){
+						value += 1;
+						if (board[n][1] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n-1][1] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n-1][0] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n+1][1] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n+1][0] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+					}
+					
+					if (board[n][heuristicCol-1] == (isMyTurn ? 2: 1)){
+						value += 1;
+						if (board[n-1][heuristicCol-1] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n-1][heuristicCol-2] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n-1][] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n+1][1] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+						if (board[n+1][0] == (isMyTurn ? 2: 1)){
+							value += 1;
+						}
+					}
+					if (board[n][heuristicCol-1] == (isMyTurn ? 2: 1)){
+						value += 1;
+					}
+				}
+			}
+			else{
+				if(board[])
+			}
+		}
+		return value;
 	}
 	
 	/**
@@ -116,10 +279,13 @@ public class Player {
 	 * @param action POP or PUSH
 	 * @param isMyTurn True if it is my turn
 	 */
-	public int[][] getHeuristicBoard(int[][] board, int position, int action, boolean isMyTurn) {
-		// TODO: Use this method to compare boards
-		throw new UnsupportedOperationException();
-	}
+//	public int[][] getHeuristicBoard(int[][] board, int position, int action, boolean isMyTurn) {
+//		// TODO: Use this method to compare boards
+//		//throw new UnsupportedOperationException();
+//		updateBoard(board, position, action, isMyTurn);
+//		int[][] heuristicBoard = board;
+//		return heuristicBoard;
+//	}
 	
 	public static void main(String[] args) throws IOException {
 		Player rp=new Player();
